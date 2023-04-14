448aa84621da711f02c1d00191534bcd2a62a5c6
Zeinab Abdien <suzanabdien15@gmail.com>
2023-04-15 00:31:08 +0200

    initial commit

---
+++ app/src/main/java/com/example/zoomimage/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.zoomimage
+
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), ScaleGestureDetector.OnScaleGestureListener {
+
+    private lateinit var imageView: ImageView
+    private lateinit var scaleGestureDetector: ScaleGestureDetector
+    private var scaleFactor = 1.0f
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.image_zoom)
+        scaleGestureDetector = ScaleGestureDetector(this, this)
+
+    }
+
+    override fun onScale(detector: ScaleGestureDetector): Boolean {
+        Log.d("suzan" , "onScale")
+
+        Log.d("suzan" , detector.toString())
+
+        detector?.let {
+            scaleFactor *= it.scaleFactor
+            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 10.0f))
+            imageView.scaleX = scaleFactor
+            imageView.scaleY = scaleFactor
+        }
+        return true
+    }
+
+    override fun onScaleBegin(p0: ScaleGestureDetector): Boolean {
+
+        Log.d("suzan" , "onScaleBegin")
+
+        return true
+    }
+
+    override fun onScaleEnd(p0: ScaleGestureDetector) {
+        Log.d("suzan" , "onScaleEnd")
+
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        scaleGestureDetector.onTouchEvent(event)
+        return true
+    }
+
+}
